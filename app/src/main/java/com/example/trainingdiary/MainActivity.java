e676598499547b986e775ed6302accf5f83a83ca
Justyna Dudar <justyna.dudar1@gmail.com>
2022-01-17 17:10:40 +0100

    registration form view

--- app/src/main/java/com/example/trainingdiary/MainActivity.java
+++ app/src/main/java/com/example/trainingdiary/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.trainingdiary;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void onClickRegisterBtn(View view) {
+        EditText edtTxtName = findViewById(R.id.edtTxtName);
+        EditText edtTxtSurname = findViewById(R.id.edtTxtSurname);
+        EditText edtTxtEmail = findViewById(R.id.edtTxtEmail);
+
+        TextView txtName = findViewById(R.id.txtName);
+        TextView txtSurname = findViewById(R.id.txtSurname);
+        TextView txtEmail = findViewById(R.id.txtEmail);
+
+        txtName.setText(edtTxtName.getText().toString());
+        txtSurname.setText(edtTxtSurname.getText().toString());
+        txtEmail.setText(edtTxtEmail.getText().toString());
     }
 }
